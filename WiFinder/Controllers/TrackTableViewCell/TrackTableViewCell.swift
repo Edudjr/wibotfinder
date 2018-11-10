ae2d0b38bf989b769d9b3f8c29d0f60b4a80999a
Eduardo Domene Junior <eduardo.djr@hotmail.com>
2018-11-10 14:55:14 -0200

    Added spring animation

--- WiFinder/Controllers/TrackTableViewCell/TrackTableViewCell.swift
+++ WiFinder/Controllers/TrackTableViewCell/TrackTableViewCell.swift
@@ -14,4 +14,33 @@ class TrackTableViewCell: UITableViewCell {
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var icon: UIImageView!
     
+    override func layoutSubviews() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        thumbnail.isUserInteractionEnabled = true
+        thumbnail.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        animateWidth(-20) { _ in }
+    }
+    
+    func animateWidth(_ width: CGFloat, completion: @escaping(Bool) -> Void) {
+        let duration = 1.0
+        let delay = 0.0
+        let damping = CGFloat(0.4)
+        let velocity = CGFloat(1.0)
+        let options = UIViewAnimationOptions.curveEaseIn
+        self.thumbnail.frame.size.width += width
+        
+        UIView.animate(withDuration:duration,
+                       delay: delay,
+                       usingSpringWithDamping: damping,
+                       initialSpringVelocity: velocity,
+                       options: options,
+                       animations: {
+                        self.thumbnail.frame.size.width -= width
+        }, completion: { success in
+            completion(success)
+        })
+    }
 }
