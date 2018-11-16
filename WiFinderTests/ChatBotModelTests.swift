040ecb4996cb1fea3ac94bfaba559df588e84cf8
Eduardo Domene Junior <eduardo.djr@hotmail.com>
2018-11-16 15:11:08 -0200

    Added more tests

--- WiFinderTests/ChatBotModelTests.swift
+++ WiFinderTests/ChatBotModelTests.swift
@@ -29,29 +29,42 @@ class ChatBotModelTests: XCTestCase {
         let _ = chatBotModel.getWelcomeMessage()
         let response = chatBotModel.getResponseFor(message: "movies")
         XCTAssertEqual(response, "Movies are great! What movie should I search for?")
+        XCTAssertEqual(chatBotModel.getCurrentStep(), ChatBotSteps.query)
     }
     
     func testTvShowCategory1() {
         let _ = chatBotModel.getWelcomeMessage()
         let response = chatBotModel.getResponseFor(message: "tv shows")
         XCTAssertEqual(response, "Hnmm, tv shows! Which one would you like to search?")
+        XCTAssertEqual(chatBotModel.getCurrentStep(), ChatBotSteps.query)
     }
     
     func testTvShowCategory2() {
         let _ = chatBotModel.getWelcomeMessage()
         let response = chatBotModel.getResponseFor(message: "shows")
         XCTAssertEqual(response, "Hnmm, tv shows! Which one would you like to search?")
+        XCTAssertEqual(chatBotModel.getCurrentStep(), ChatBotSteps.query)
     }
     
     func testMusicCategory() {
         let _ = chatBotModel.getWelcomeMessage()
         let response = chatBotModel.getResponseFor(message: "music")
         XCTAssertEqual(response, "I like music! Which song or artist should I search for?")
+        XCTAssertEqual(chatBotModel.getCurrentStep(), ChatBotSteps.query)
     }
     
     func testWrongCategory() {
         let _ = chatBotModel.getWelcomeMessage()
         let response = chatBotModel.getResponseFor(message: "skibidi")
         XCTAssertEqual(response, "Sorry. Try something like Movies, TV Shows or Music!")
+        XCTAssertEqual(chatBotModel.getCurrentStep(), ChatBotSteps.category)
+    }
+    
+    func testMovieCategoryAndQuery() {
+        let _ = chatBotModel.getWelcomeMessage()
+        let _ = chatBotModel.getResponseFor(message: "movies")
+        let response = chatBotModel.getResponseFor(message: "john wick")
+        XCTAssertEqual(response, "Getting results for \"john wick\"")
+        XCTAssertEqual(chatBotModel.getCurrentStep(), ChatBotSteps.last)
     }
 }
